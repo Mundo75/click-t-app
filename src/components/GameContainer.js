b632a8f5f571e07fb17d3b0ccea7b300a5159e86
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-06-28 15:17:13 -0700

    base components and style coded

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,1 +1,5 @@
-import React from "react";
+import React, {Component} from "react";
+import Game from "./Game";
+import Mrt from "./../mrt.json";
+import shuffle from "shuffle-array";
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,9 @@
 import React from "react";
+
+const Header = props => (
+    <div className="container text-center" id="header-container">
+        <h1 id="title"><b>Click "T" Game</b></h1>
+    </div>
+);
+
+export default Header;
